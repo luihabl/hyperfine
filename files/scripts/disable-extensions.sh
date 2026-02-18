1d8327c45a4f1fced7da1f75c00eb88dec3e8a80
Lui Habl <Lui Habl>
2026-02-18 00:09:22 -0300

    Disabling extensions

---
+++ files/scripts/disable-extensions.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo 'Disabling gnome extensions'
+
+gnome-extensions disable blur-my-shell@aunetx
+gnome-extensions disable appindicatorsupport@rgcjonas.gmail.com
+gnome-extensions disable caffeine@patapon.info
+gnome-extensions disable dash-to-dock@micxgx.gmail.com
+gnome-extensions disable gsconnect@andyholmes.github.io
+gnome-extensions disable logomenu@aryan_k
+gnome-extensions disable search-light@icedman.github.com
---
+++ files/scripts/remove-starship.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+rm -f /etc/profile.d/90-bluefin-starship.sh
+rm -f /usr/bin/starship
